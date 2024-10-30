4eb9ca4462cbc5b249229dee12b3ea2f7670e81e
srysis <jashandenis@gmail.com>
2024-10-30 23:49:42 +0100

    changed style imports - now uses .scss files

--- site_1/src/index.jsx
+++ site_1/src/index.jsx
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom/client';
 
 import AppRoot from "./AppRoot.jsx"
 
-import './style/style.css';
-import './style/style-phone.css';
+import './style/style.scss';
+import './style/style-phone.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
